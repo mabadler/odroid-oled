3f0eb2a4489f68ac86be34922f14bb5e5048a807
Max Bowman <mabowman200@gmail.com>
2018-03-31 11:28:00 -0500

    Initial commit.

---
+++ Adafruit_GFX/Adafruit_GFX.h
@@ -0,0 +1,69 @@
+#define swap(a, b) { int t = a; a = b; b = t; }
+
+class Adafruit_GFX {
+
+ public:
+
+  Adafruit_GFX(int w, int h); // Constructor
+
+  // This MUST be defined by the subclass:
+  virtual void drawPixel(int x, int y, int color) = 0;
+
+  // These MAY be overridden by the subclass to provide device-specific
+  // optimized code.  Otherwise 'generic' versions are used.
+  virtual void
+    drawLine(int x0, int y0, int x1, int y1, int color),
+    drawFastVLine(int x, int y, int h, int color),
+    drawFastHLine(int x, int y, int w, int color),
+    drawRect(int x, int y, int w, int h, int color),
+    fillRect(int x, int y, int w, int h, int color),
+    fillScreen(int color),
+    invertDisplay(bool i);
+
+  // These exist only with Adafruit_GFX (no subclass overrides)
+  void
+    drawCircle(int x0, int y0, int r, int color),
+    drawCircleHelper(int x0, int y0, int r, int cornername,
+      int color),
+    fillCircle(int x0, int y0, int r, int color),
+    fillCircleHelper(int x0, int y0, int r, int cornername,
+      int delta, int color),
+    drawTriangle(int x0, int y0, int x1, int y1,
+      int x2, int y2, int color),
+    fillTriangle(int x0, int y0, int x1, int y1,
+      int x2, int y2, int color),
+    drawRoundRect(int x0, int y0, int w, int h,
+      int radius, int color),
+    fillRoundRect(int x0, int y0, int w, int h,
+      int radius, int color),
+    drawBitmap(int x, int y, const int *bitmap,
+      int w, int h, int color),
+    drawChar(int x, int y, unsigned char c, int color,
+      int bg, int size),
+    setCursor(int x, int y),
+    setTextColor(int c),
+    setTextColor(int c, int bg),
+    setTextSize(int s),
+    setTextWrap(bool w),
+    setRotation(int r);
+
+  int
+    height(void),
+    width(void);
+
+  int getRotation(void);
+
+ protected:
+  const int
+    WIDTH, HEIGHT;   // This is the 'raw' display w/h - never changes
+  int
+    _width, _height, // Display w/h as modified by current rotation
+    cursor_x, cursor_y;
+  int
+    textcolor, textbgcolor;
+  int
+    textsize,
+    rotation;
+  bool
+    wrap; // If set, 'wrap' text at right edge of display
+};
---
+++ oled.h
@@ -0,0 +1,94 @@
+/*********************************************************************
+This is a library for our Monochrome OLEDs based on SSD1306 drivers
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+
+Modified for use with Odroid by Far Horizons (2018)
+*********************************************************************/
+
+#include "Adafruit_GFX/Adafruit_GFX.h"
+
+#define BLACK 0
+#define WHITE 1
+
+#define I2C_ADDRESS   0x3C
+
+#define LCDWIDTH 128
+#define LCDHEIGHT 64
+
+
+#define SETCONTRAST 0x81
+#define DISPLAYALLON_RESUME 0xA4
+#define DISPLAYALLON 0xA5
+#define NORMALDISPLAY 0xA6
+#define INVERTDISPLAY 0xA7
+#define DISPLAYOFF 0xAE
+#define DISPLAYON 0xAF
+
+#define SETDISPLAYOFFSET 0xD3
+#define SETCOMPINS 0xDA
+
+#define SETVCOMDETECT 0xDB
+
+#define SETDISPLAYCLOCKDIV 0xD5
+#define SETPRECHARGE 0xD9
+
+#define SETMULTIPLEX 0xA8
+
+#define SETLOWCOLUMN 0x00
+#define SETHIGHCOLUMN 0x10
+
+#define SETSTARTLINE 0x40
+
+#define MEMORYMODE 0x20
+
+#define COMSCANINC 0xC0
+#define COMSCANDEC 0xC8
+
+#define SEGREMAP 0xA0
+
+#define CHARGEPUMP 0x8D
+
+#define EXTERNALVCC 0x1
+#define SWITCHCAPVCC 0x2
+
+// Scrolling #defines
+#define ACTIVATE_SCROLL 0x2F
+#define DEACTIVATE_SCROLL 0x2E
+#define SET_VERTICAL_SCROLL_AREA 0xA3
+#define RIGHT_HORIZONTAL_SCROLL 0x26
+#define LEFT_HORIZONTAL_SCROLL 0x27
+#define VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL 0x29
+#define VERTICAL_AND_LEFT_HORIZONTAL_SCROLL 0x2A
+
+class Oled : public Adafruit_GFX {
+ public:
+  Oled(int RST);
+
+  void begin(int switchvcc = SWITCHCAPVCC, int i2caddr = I2C_ADDRESS);
+  void command(int c);
+  void data(int c);
+
+  void clearDisplay(void);
+  void invertDisplay(int i);
+  void display();
+
+  void startscrollright(int start, int stop);
+  void startscrollleft(int start, int stop);
+
+  void startscrolldiagright(int start, int stop);
+  void startscrolldiagleft(int start, int stop);
+  void stopscroll(void);
+
+  void drawPixel(int16_t x, int16_t y, uint16_t color);
+
+ private:
+  int _i2caddr;
+
+};
